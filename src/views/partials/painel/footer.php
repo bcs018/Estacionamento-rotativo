acae995529254152e809271937330af9efc63997
Bruno César <57082742+bcs018@users.noreply.github.com>
2022-02-01 15:25:27 -0300

    Adicionado header e footer

---
+++ src/views/partials/painel/footer.php
@@ -0,0 +1,50 @@
+<footer class="main-footer">
+  <strong>Copyright &copy; <?= date('Y'); ?> - EstacionaCar - </strong> 
+    Todos direitos reservados
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Versão</b> 1.0.0
+  </div>
+</footer>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="<?php echo _BASE_ASS_ ?>painel/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo _BASE_ASS_ ?>painel/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo _BASE_ASS_ ?>painel/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo _BASE_ASS_ ?>painel/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo _BASE_ASS_ ?>painel/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="<?php echo _BASE_ASS_ ?>painel/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?php echo _BASE_ASS_ ?>painel/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo _BASE_ASS_ ?>painel/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?php echo _BASE_ASS_ ?>painel/moment/moment.min.js"></script>
+<script src="<?php echo _BASE_ASS_ ?>painel/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?php echo _BASE_ASS_ ?>painel/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="<?php echo _BASE_ASS_ ?>painel/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?php echo _BASE_ASS_ ?>painel/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo _BASE_ASS_ ?>painel/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo _BASE_ASS_ ?>painel/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo _BASE_ASS_ ?>painel/dist/js/pages/dashboard.js"></script>
+</body>
+</html>
