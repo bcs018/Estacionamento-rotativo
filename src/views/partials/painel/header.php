902325676010508ebaa0fde41a5a8bb7f3bd0d3d
Bruno César <57082742+bcs018@users.noreply.github.com>
2022-02-07 17:24:59 -0300

    correção de erro

--- src/views/partials/painel/header.php
+++ src/views/partials/painel/header.php
@@ -6,8 +6,7 @@
     <title>Painel de controle | <?= $title; ?></title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?= _BASE_ASS_ ?>painel/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
